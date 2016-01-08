42b2ee271cf9b211d825a6b54240e77ab929d92e
Armando-Satellite <apiconz@gmail.com>
2016-01-08 08:44:28 -0500

    my firs commit

---
+++ ex1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+#print "Hello World!"
+#print "Hello Again"
+#print "I like typing this."
+#print "This is fun."
+#print 'Yay! Printing.'
+#print "I'd much rather you 'not'."
+#print 'I "said" do no touch this.'
+print "Hola este es mi mundo!"
