c34ce73b164c40038fa91c24027e7df140331797
Armando <apiconz@gmail.com>
2016-01-09 19:55:37 -0500

    Más ejercicios resueltos

---
+++ ex6.py
@@ -0,0 +1,24 @@
+# declaring some variables whos contain strings
+x = "There are %d types of people." % 10
+binary = "binary"
+do_not = "don't"
+y = "Those who know %s and those who %s." % (binary, do_not)
+
+# printing variables x and y
+print x
+print y
+
+# joining a string with variables
+print "I said: %r." % x
+print "I also said: '%s'." % y
+
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! %r"
+
+# when we print a boolean value, this is printed as that is
+print joke_evaluation % hilarious
+
+w = "This is the left side of..."
+e = "a string with a right side."
+
+print w + e
